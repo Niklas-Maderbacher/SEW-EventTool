6619078c398a467954ed327e26f56e4928414aed
Niklas-Maderbacher <maderb190007@sr.htlweiz.at>
2025-11-17 12:27:52 +0100

    Removed blank space in Version name

--- backend/scripts/alembic_runner.sh
+++ backend/scripts/alembic_runner.sh
@@ -3,7 +3,7 @@
 if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <alembic command and parameters>"
     echo "Example: $0 upgrade head"
-    echo "         $0 revision --autogenerate -m \"init migration\""
+    echo "         $0 revision --autogenerate -m \"init-migration\""
     exit 1
 fi
 
